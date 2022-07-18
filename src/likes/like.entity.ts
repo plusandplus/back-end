380e73f5fed57c896b864f22e84d16ad6bf905da
ygreenb <nhj7911@gmail.com>
2022-07-19 04:15:04 +0900

    [#15] Feat: like api 구현

--- src/likes/like.entity.ts
+++ src/likes/like.entity.ts
@@ -14,19 +14,23 @@ export class Like extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column({ type: 'int', comment: '숙소아이디' })
+  station_id: number;
+
+  @Column({ type: 'int', comment: '유저아이디' })
+  user_id: number;
+
   @ManyToOne(() => Station, {
     onDelete: 'CASCADE',
     nullable: false,
   })
   @JoinColumn({ name: 'station_id' })
-  station_id: Station;
+  station: Station;
 
-  @Column({ type: 'int', comment: '유저아이디' })
-  user_id: number;
-  //   @ManyToOne(() => User, {
-  //     onDelete: 'CASCADE',
-  //     nullable: false,
-  //   })
-  //   @JoinColumn({ name: 'user_id' })
-  //   user_id: Station;
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
 }
--- src/likes/like.repository.ts
+++ src/likes/like.repository.ts
@@ -1,5 +1,24 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, Repository } from 'typeorm';
+import { resourceLimits } from 'worker_threads';
 import { Like } from './like.entity';
 
 @EntityRepository(Like)
-export class LikeRepository extends Repository<Like> {}
+export class LikeRepository extends Repository<Like> {
+  async getLikesByUser(id: number): Promise<Like[]> {
+    const result = getRepository(Like)
+      .createQueryBuilder('like')
+      .select('station_id')
+      .leftJoinAndSelect('like.station', 'station')
+      .where(`like.user_id = ${id}`);
+    return await result.getRawMany();
+  }
+
+  async getLikeCountByStation(id: number): Promise<object> {
+    const result = await getRepository(Like)
+      .createQueryBuilder('like')
+      .select('COUNT(*) as like_cnt')
+      .where(`like.station_id = ${id}`)
+      .getRawOne();
+    return result;
+  }
+}
