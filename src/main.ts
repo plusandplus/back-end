d6610750d0f09312e687fbcf76d420abf62aa9b0
ygreenb <nhj7911@gmail.com>
2022-08-10 01:41:42 +0900

    [#5] Feat: 다중 파일 업로드 구현

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.AWS_ACCESS_KEY);
+  console.log('1');
   const app = await NestFactory.create(AppModule, { cors: true });
+  console.log(process.env.AWS_ACCESS_KEY);
+  console.log('2');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.SERVER_PORT);
   Logger.log(`Application running on port ${process.env.SERVER_PORT}`);
--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -1,43 +1,32 @@
 import {
   Controller,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { Express } from 'express';
-import * as AWS from 'aws-sdk';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { UploadsService } from './uploads.service';
 
 @Controller('uploads')
 export class UploadsController {
+  constructor(private readonly uploadsService: UploadsService) {}
+
   @Post('')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    AWS.config.update({
-      region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY,
-      },
-    });
-    try {
-      const key = `${Date.now() + file.originalname}`;
-      const upload = await new AWS.S3()
-        .putObject({
-          Key: key,
-          Body: file.buffer,
-          Bucket: process.env.AWS_BUCKET_NAME,
-        })
-        .promise();
-      console.log(upload);
-      const imgurl = process.env.AWS_CLOUDFRONT + key;
-      return Object.assign({
-        statusCode: 201,
-        message: `이미지 등록 성공`,
-        data: { url: imgurl },
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  @UseInterceptors(FilesInterceptor('files', 10))
+  async uploadFile(@UploadedFiles() files) {
+    console.log(files);
+    const imgurl: string[] = [];
+    await Promise.all(
+      files.map(async (file: Express.Multer.File) => {
+        const key = await this.uploadsService.uploadImage(file);
+        imgurl.push(process.env.AWS_CLOUDFRONT + key);
+      }),
+    );
+
+    return {
+      statusCode: 201,
+      message: `이미지 등록 성공`,
+      data: imgurl,
+    };
   }
 }
