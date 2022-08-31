edad89211fd565ec57549109cad300aa415bacdf
ygreenb <nhj7911@gmail.com>
2022-08-31 17:52:45 +0900

    Fix: fix settings for heroku deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.SERVER_PORT);
+  await app.listen(process.env.PORT || process.env.SERVER_PORT);
   Logger.log(`Application running on port ${process.env.SERVER_PORT}`);
 }
 bootstrap();
