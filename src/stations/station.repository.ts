3d18453ebdc7ed545fd5968e39838e1064bc7bc7
ygreenb <nhj7911@gmail.com>
2022-09-08 04:10:29 +0900

    Fix: limit, offset -> take, skip

--- src/stations/station.repository.ts
+++ src/stations/station.repository.ts
@@ -70,7 +70,7 @@ export class StationRepository extends Repository<Station> {
       const themearr = themeIds.split(',');
       result.andWhere('theme.id IN (:...themeIds)', { themeIds: themearr });
     }
-    // result.limit(limit).offset(offset);
+    result.take(limit).skip(offset);
     console.log(result.getQuery());
 
     const [stations, count] = await result
@@ -167,7 +167,7 @@ export class StationRepository extends Repository<Station> {
         .groupBy('station.id')
         .having('count(*)-count(o.id)>0');
     }
-    // result.limit(limit).offset(offset);
+    result.take(limit).skip(offset);
     console.log(result.getQuery());
     const [stations, count] = await result.getManyAndCount();
     return { count, stations };
